04e96639acaf7efabdffaf7196855d83c9adaf08
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-06-11 14:05:50 +0200

    update jdl generator to only support moteurlite (moteur2 is now fully deprecated)

--- src/test/java/fr/insalyon/creatis/gasw/plugin/executor/dirac/execution/DiracJdlGeneratorTest.java
+++ src/test/java/fr/insalyon/creatis/gasw/plugin/executor/dirac/execution/DiracJdlGeneratorTest.java
@@ -10,8 +10,6 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -20,7 +18,6 @@ import org.junit.jupiter.api.Test;
 
 import fr.insalyon.creatis.gasw.GaswException;
 import fr.insalyon.creatis.gasw.plugin.executor.dirac.DiracConfiguration;
-import fr.insalyon.creatis.gasw.plugin.executor.dirac.DiracConstants;
 
 @DisplayName("Dirac JDL generation tests")
 public class DiracJdlGeneratorTest {
@@ -39,7 +36,9 @@ public class DiracJdlGeneratorTest {
             false,
             true,
             Arrays.asList("first.banned.site", "second.banned.site"),
-            Arrays.asList("Any", "Multiple"));
+            Arrays.asList("Any", "Multiple"),
+            Arrays.asList("Strasbourg"),
+            Arrays.asList());
     }
 
     @AfterAll
@@ -67,49 +66,13 @@ public class DiracJdlGeneratorTest {
     }
 
     @Test
-    @DisplayName("Creation of JDL")
-    public void creationJdl() throws GaswException {
-        // Given
-        DiracJdlGenerator generator = DiracJdlGenerator.getInstance();
-        Map<String, String> envVariables = new HashMap<>();
-        // The generator is a singleton instance, and the order of the tests is
-        // unknown.  So that other tests have no impact, we need to overwrite
-        // the previous tag.
-        envVariables.put(DiracConstants.ENV_TAGS, "");
-
-        // When
-        String result = generator.generate("scriptName.sh", envVariables, false);
-
-        // Then
-        System.out.println(result);
-        assertEquals(10, result.split("\n").length);
-        assertTrue(result.contains("JobName         = \"scriptName - GASW-Dirac-Plugin\";"));
-        assertTrue(result.contains("Executable      = \"scriptName.sh\";"));
-        assertTrue(result.contains("StdOutput       = \"std.out\";"));
-        assertTrue(result.contains("StdError        = \"std.err\";"));
-        assertTrue(result.contains("InputSandbox    = {\""));
-        assertTrue(result.contains("OutputSandbox   = {\"std.out\", \"std.err\", \"scriptName.sh.provenance.json\"};"));
-        assertTrue(result.contains("CPUTime         = \"1800\";"));
-        assertTrue(result.contains("Priority        = 0;"));
-        assertTrue(result.contains("Site            = \"\";"));
-        assertTrue(result.contains("BannedSite      = \"first.banned.site,second.banned.site\";"));
-
-        String inputsLine = result.lines().filter(l -> l.startsWith("InputSandbox")).findAny().get();
-        String[] inputs = inputsLine.split(",");
-        assertEquals(1, inputs.length);
-        assertTrue(inputs[0].endsWith("/sh/scriptName.sh\"};"));
-    }
-
-    @Test
     @DisplayName("Creation of JDL with MoteurLite")
     public void creationJdlWithMoteurLite() throws GaswException {
         // Given
         DiracJdlGenerator generator = DiracJdlGenerator.getInstance();
-        Map<String, String> envVariables = new HashMap<>();
-        envVariables.put(DiracConstants.ENV_TAGS, "");
         
         // When
-        String result = generator.generate("scriptName.sh", envVariables, true);
+        String result = generator.generate("scriptName.sh");
         
         // Then
         System.out.println(result);
@@ -123,7 +86,7 @@ public class DiracJdlGeneratorTest {
         assertTrue(result.contains("OutputSandbox   = {\"std.out\", \"std.err\", \"scriptName.sh.provenance.json\"};"));
         assertTrue(result.contains("CPUTime         = \"1800\";"));
         assertTrue(result.contains("Priority        = 0;"));
-        assertTrue(result.contains("Site            = \"\";"));
+        assertTrue(result.contains("Site            = \"Strasbourg\";"));
         assertTrue(result.contains("BannedSite      = \"first.banned.site,second.banned.site\";"));
     
         // Check MoteurLite-specific components
@@ -135,23 +98,5 @@ public class DiracJdlGeneratorTest {
         assertTrue(inputs[2].endsWith("/config/scriptName-configuration.sh\""));
         System.out.println("inputs[3] :" + inputs[3]);
         assertTrue(inputs[3].endsWith("/workflow.json\"};"));
-    }
-    
-
-
-    @Test
-    @DisplayName("Creation of JDL with a tag")
-    public void creationJdlWithTag() throws GaswException {
-        // Given
-        DiracJdlGenerator generator = DiracJdlGenerator.getInstance();
-        Map<String, String> envVariables = new HashMap<>();
-        envVariables.put(DiracConstants.ENV_TAGS, "creatisGpu");
-
-        // When
-        String result = generator.generate("scriptName", envVariables, false);
-
-        // Then
-        assertEquals(11, result.split("\n").length);
-        assertTrue(result.contains("Tags            = \"creatisGpu\";"));
     }
 }
