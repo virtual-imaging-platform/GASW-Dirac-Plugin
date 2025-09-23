d05d06946eb3b76eda4bc045b45c1bcd210d7e31
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-09-23 11:00:47 +0200

    fix tests

--- src/test/java/fr/insalyon/creatis/gasw/plugin/executor/dirac/execution/DiracJdlGeneratorTest.java
+++ src/test/java/fr/insalyon/creatis/gasw/plugin/executor/dirac/execution/DiracJdlGeneratorTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import fr.insalyon.creatis.gasw.GaswConfiguration;
 import fr.insalyon.creatis.gasw.GaswException;
 import fr.insalyon.creatis.gasw.plugin.executor.dirac.DiracConfiguration;
 
@@ -24,6 +25,8 @@ public class DiracJdlGeneratorTest {
 
     @BeforeAll
     public static void createConfiguration() {
+        // to avoid containers.runtime strict checking
+        GaswConfiguration.setStrict(false);
         DiracConfiguration.setConfiguration(
             "host",
             "defaultPool",
